acc3e6696bae9f6d28f04c341233676aaaf660f6
Aditya <adityamishra25034@gmail.com>
2025-12-16 23:03:41 +0530

    basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 12345;
+let accountEmail = "aditya@google.com";
+var accountPassword = "12345";
+accountCity = "New York";
+
+//accountId = 67890; // This will throw an error because accountId is a constant
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
